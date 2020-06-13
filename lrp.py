2d3c6c166d2637109b3dd7a030f981cd0875a82f
LearnerYme <49382488+LearnerYme@users.noreply.github.com>
2020-06-13 12:19:35 +0800

    update lrp.py

--- lrp.py
+++ lrp.py
@@ -1,10 +1,7 @@
 import torch
 
-'''
-this tool box support pytorch only, yet.
-'''
+__version__ = '1.4\nfix: conv method.\nThe codes now go more beautiful.'
 
-__version__ = '1.3\nFix: R of different layers differ. -> Now they are the same. (if ignore epsilon)'
 class lrp():
     def __init__(self,epsilon):
         self.epsilon = epsilon
@@ -12,7 +9,6 @@ class lrp():
         self.x = None
         self.y = None
         self.weight = None
-        self.bias = None
         self.hstride = None
         self.wstride = None
         self.hpool = None
@@ -23,16 +19,16 @@ class lrp():
         self.x = None
         self.y = None
         self.weight = None
-        self.bias = None
         self.hstride = None
         self.wstride = None
         self.hpool = None
         self.wpool = None
         return
 
-    def R0(self,net,inputs):
-        y = net.forward(inputs)
-        y = torch.nn.functional.softmax(y,1)
+    def Rinit(self,net,inputs,softmax=True):
+        y = net(inputs)
+        if softmax:
+            y = torch.nn.functional.softmax(y,1)
         _,pred = torch.max(y.detach(),1)
         self.R = torch.zeros_like(y)
         mask = torch.zeros_like(y)
@@ -44,8 +40,8 @@ class lrp():
     def set_(self,layer,*arg):
         '''
         layer: 'conv', 'fc', 'avepool', 'maxpool', 'flatten'
-        conv: x, weight, bias, hstride, wstride
-        fc: x, weight, bias
+        conv: x, weight,  hstride, wstride
+        fc: x, weight
         avepool/maxpool: x, y, hpool, wpool, hstride, wstride
         flatten: x
         '''
@@ -54,13 +50,13 @@ class lrp():
         if layer not in ['conv','fc','avepool','maxpool','flatten']:
             raise Exception('cannot manipulate "%s" layer.'%layer)
         if layer == 'conv':
-            if l != 5:
-                raise Exception('convolution layer should have had 5 arguments: x, weight, bias, hstride, wstride, but %d are given.'%l)
-            self.x, self.weight, self.bias, self.hstride, self.wstride = arg
+            if l != 4:
+                raise Exception('convolution layer should have had 4 arguments: x, weight, hstride, wstride, but %d are given.'%l)
+            self.x, self.weight, self.hstride, self.wstride = arg
         elif layer == 'fc':
-            if l != 3:
-                raise Exception('fc layer should have had 3 arguments: x, weight, bias, but %d are given.'%l)
-            self.x, self.weight, self.bias= arg
+            if l != 2:
+                raise Exception('fc layer should have had 2 arguments: x, weight, but %d are given.'%l)
+            self.x, self.weight = arg
         elif layer == 'flatten':
             if l != 1:
                 raise Exception('flatten layer should have had 1 arguments: x, but %d are given.'%l)
@@ -79,21 +75,17 @@ class lrp():
         #R [N,n]
         with torch.no_grad():
             w = self.weight#[n,a]
-            b = self.bias#[n]
             x = self.x#[N,a]
-            b.unsqueeze_(0)
-            b.unsqueeze_(0)#[1,1,n]
             w = torch.transpose(w,0,1)#[a,n]
             x.unsqueeze_(2)#[N,a,1]
             w.unsqueeze_(0)#[1,a,n]
             z = w * x#[N,a,n]
-            #zs = z.sum(dim=1,keepdim=True) + b#[N,1,n]
             zs = z.sum(dim=1,keepdim=True)#[N,1,n]
             zs += self.epsilon * ((zs>=0)*2-1)#[N,1,n]
             self.R = (z / zs * self.R.unsqueeze(1)).sum(dim=2)#[N,a]
         return
 
-    def fc_v(self):
+    def fc_alter(self):
         with torch.no_grad():
             w = self.weight
             b = self.bias
@@ -111,8 +103,6 @@ class lrp():
                     z[:,i] += up / down * self.R[:,j]
             self.R = z
         return
-
-                    
 
     def flatten(self):
         with torch.no_grad():
@@ -161,7 +151,6 @@ class lrp():
                 for j in range(Wout):
                     z = self.x[:,:,i*hstride:i*hstride+hpool,j*wstride:j*wstride+wpool]
                     zs = z.sum(dim=(2,3),keepdims=True)
-                    #zs = zs / (H * W)
                     zs += self.epsilon * ((zs>=0)*2-1)
                     Rx[:,:,i*hstride:i*hstride+hpool,j*wstride:j*wstride+wpool] += (z / zs) * self.R[:,:,i:i+1,j:j+1]
             self.R = Rx
@@ -171,7 +160,6 @@ class lrp():
         #N: batch size
         with torch.no_grad():
             w = self.weight#[cout,cin,hfilter,wfilter]
-            b = self.bias#[cin]
             x = self.x
             cout, cin, hfilter, wfilter = w.shape
             hstride = self.hstride
@@ -180,14 +168,13 @@ class lrp():
             Hout = (H - hfilter) // hstride + 1
             Wout = (W - wfilter) // wstride + 1
             Rx = torch.zeros_like(self.x)#[N,cin,H,W]
-            w = w.unsqueeze(0)#[1,cout,cin,hfilter,wfilter]
-            x = x.unsqueeze(1)#[N,1,cin,H,W]
-            b = b.view(1,-1,1,1,1)
+            w.unsqueeze_(0)#[1,cout,cin,hfilter,wfilter]
+            x.unsqueeze_(1)#[N,1,cin,H,W]
             R = self.R.unsqueeze(2)#[N,cout,1,Hout,Wout]
             for i in range(Hout):
                 for j in range(Wout):
                     z = w * x[:,:,:,i*hstride:i*hstride+hfilter,j*wstride:j*wstride+wfilter]
-                    zs = z.sum(dim=(2,3,4),keepdims=True) + b
+                    zs = z.sum(dim=(2,3,4),keepdims=True)
                     zs += self.epsilon * ((zs>=0)*2-1)
                     Rx[:,:,i*hstride:i*hstride+hfilter,j*wstride:j*wstride+wfilter] += ((z / zs) * R[:,:,:,i:i+1,j:j+1]).sum(dim=1)
             self.R = Rx
